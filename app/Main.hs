cc483231338dc52049468fbf4ba3a20c2331c056
Alexander Svyazin <guybrush@live.ru>
2025-01-08 10:36:27 +0500

    split to functions

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,84 @@
 module Main where
 
-import qualified Data.ByteString.Lazy as BL
 import Control.Monad (forM_)
-import Tracing
+import Data.ByteString.Lazy qualified as BL
 import Data.Map as Map
+import Data.Word
+import Instructions
+import LLVM.Core (CodeGenModule, Function, IsConst (constOf), Linkage (PrivateLinkage), createGlobal)
+import Tracing
+
+newtype TraceFunction = TraceFunction {funcStart :: Address}
+
+data InstructionData = InstructionData
+  { iInstruction :: !Instruction,
+    iNextAddresses :: ![Address]
+  }
+
+data TraceResult = TraceResult
+  { instructions :: !(Map Address InstructionData),
+    functions :: !(Map Address TraceFunction)
+  }
+
+disasmToFunctions :: String -> IO (Either String TraceResult)
+disasmToFunctions filename = do
+  bl <- BL.readFile filename
+  let callback
+        TraceResult
+          { instructions = curInstructions,
+            functions = curFunctions
+          }
+        TraceContext
+          { current =
+              TraceInstruction
+                { address = addr,
+                  instruction = inst
+                },
+            previous = prevInst,
+            next = nextAddrs
+          } =
+          let newInstructions =
+                Map.insert addr (InstructionData inst nextAddrs) curInstructions
+              addNewFunction =
+                Map.insert addr (TraceFunction addr) curFunctions
+              newFunctions = case prevInst of
+                Nothing ->
+                  addNewFunction
+                Just (TraceInstruction {address = prevAddr, instruction = prevInstruction}) ->
+                  let curFunction =
+                        curFunctions ! prevAddr
+                      addToCurFunction =
+                        Map.insert addr curFunction curFunctions
+                   in case prevInstruction of
+                        InstCall callAddr ->
+                          if callAddr == addr
+                            then addNewFunction
+                            else addToCurFunction
+                        _ ->
+                          addToCurFunction
+           in TraceResult newInstructions newFunctions
+  pure (trace bl callback (TraceResult Map.empty Map.empty))
+
+registersCount :: Int
+registersCount = 16
+
+mainFunc :: CodeGenModule (Function (IO ()))
+mainFunc = do
+  let createRegister i = do
+        globalVar <- createGlobal True PrivateLinkage (constOf (0 :: Word8))
+        pure (i, globalVar)
+  registers <- Map.fromList <$> mapM createRegister [0 .. (registersCount - 1)]
+  let createFunction instructions = do
+        undefined
+  undefined
 
 main :: IO ()
 main = do
-  bl <- BL.readFile "tetris.ch8"
-  let callback s TraceContext { currentAddress = addr, currentInstruction = inst } =
-        Map.insert addr inst s
-  case trace bl callback Map.empty of
+  res <- disasmToFunctions "tetris.ch8"
+  case res of
     Left str -> error str
-    Right res -> forM_ (Map.assocs res) $ \(addr, inst) ->
-      putStrLn $ show addr ++ ":\t" ++ show inst
+    Right (TraceResult {instructions = instructions, functions = functions}) ->
+      forM_ (Map.assocs instructions) $ \(addr, InstructionData {iInstruction = inst}) -> do
+        let TraceFunction {funcStart = func} =
+              functions ! addr
+        putStrLn $ "(" ++ show func ++ ") " ++ show addr ++ ":\t" ++ show inst
--- app/Tracing.hs
+++ app/Tracing.hs
@@ -1,21 +1,33 @@
 module Tracing where
 
-import Control.Monad.Error.Class (MonadError(throwError))
-import Control.Monad.State (MonadState(get, put), evalStateT)
+import Control.Monad.Error.Class (MonadError (throwError))
+import Control.Monad.State (MonadState (get, put), runStateT)
 import Data.Binary.Get (runGet)
 import Data.ByteString.Lazy as BL
-import qualified Data.List as L
+import Data.List qualified as L
 import Data.Sequence as Seq
 import Data.Set as Set
 import Data.Word
 import Instructions
 
-newtype TraceState =
-  TraceState { traceI :: Maybe Address }
+newtype TraceState
+  = TraceState {traceI :: Maybe Address}
+
+data TraceInstruction = TraceInstruction
+  { address :: !Address,
+    instruction :: !Instruction
+  }
 
 data TraceContext = TraceContext
-  { currentAddress :: !Address
-  , currentInstruction :: !Instruction
+  { current :: !TraceInstruction,
+    previous :: !(Maybe TraceInstruction),
+    next :: ![Address]
+  }
+
+data TraceAddressToProcess = TraceAddressToProcess
+  { previousAddress :: !(Maybe Address),
+    traceState :: !TraceState,
+    currentAddress :: !Address
   }
 
 startAddress :: Address
@@ -23,25 +35,29 @@ startAddress = Address 0x200
 
 trace :: BL.ByteString -> (s -> TraceContext -> s) -> s -> Either String s
 trace program callback s0 =
-  evalStateT (traceIter s0 (Seq.singleton startAddress) Set.empty) (TraceState Nothing)
-  where traceIter curS Seq.Empty _ =
-          pure curS
-        traceIter curS (x :<| xs) processedAddresses = do
-          let curInst =
-                getInstByAddr x
-              nextS =
-                callback curS (TraceContext x curInst)
-              nextProcessedAddresses =
-                Set.insert x processedAddresses
-          additionalAddresses <- nextAddresses x curInst
-          let nextAddressesToProcess =
-                Seq.fromList (L.filter (`Set.notMember` processedAddresses) additionalAddresses) >< xs
-          traceIter nextS nextAddressesToProcess nextProcessedAddresses
-        getInstByAddr (Address addr) =
-          let bl =
-                BL.drop (fromIntegral (addr - startAddrValue)) program
-              (Address startAddrValue) = startAddress
-          in runGet parseInstruction bl
+  traceIter s0 (Seq.singleton (TraceAddressToProcess Nothing (TraceState Nothing) startAddress)) Set.empty
+  where
+    traceIter curS Seq.Empty _ =
+      pure curS
+    traceIter curS (TraceAddressToProcess {previousAddress = prevX, currentAddress = x, traceState = curState} :<| xs) processedAddresses = do
+      let curInst =
+            getInstByAddr x
+          prevTraceInst = do
+            prevAddr <- prevX
+            pure $ TraceInstruction prevAddr $ getInstByAddr prevAddr
+          nextProcessedAddresses =
+            Set.insert x processedAddresses
+      (additionalAddresses, nextState) <- runStateT (nextAddresses x curInst) curState
+      let nextS =
+            callback curS (TraceContext (TraceInstruction x curInst) prevTraceInst additionalAddresses)
+          nextAddressesToProcess =
+            Seq.fromList (L.map (TraceAddressToProcess (Just x) nextState) (L.filter (`Set.notMember` processedAddresses) additionalAddresses)) >< xs
+      traceIter nextS nextAddressesToProcess nextProcessedAddresses
+    getInstByAddr (Address addr) =
+      let bl =
+            BL.drop (fromIntegral (addr - startAddrValue)) program
+          (Address startAddrValue) = startAddress
+       in runGet parseInstruction bl
 
 instSize :: Word16
 instSize = 2
@@ -59,27 +75,33 @@ nextAddresses curAddr (InstCall callAddr) =
   pure [callAddr, nextInstAddr curAddr]
 nextAddresses curAddr (InstSke _ _) =
   pure [nextAddr, nextInstAddr nextAddr]
-  where nextAddr = nextInstAddr curAddr
+  where
+    nextAddr = nextInstAddr curAddr
 nextAddresses curAddr (InstSkne _ _) =
   pure [nextAddr, nextInstAddr nextAddr]
-  where nextAddr = nextInstAddr curAddr
+  where
+    nextAddr = nextInstAddr curAddr
 nextAddresses curAddr (InstSkre _ _) =
   pure [nextAddr, nextInstAddr nextAddr]
-  where nextAddr = nextInstAddr curAddr
+  where
+    nextAddr = nextInstAddr curAddr
 nextAddresses curAddr (InstSkrne _ _) =
   pure [nextAddr, nextInstAddr nextAddr]
-  where nextAddr = nextInstAddr curAddr
+  where
+    nextAddr = nextInstAddr curAddr
 nextAddresses curAddr (InstSkpr _) =
   pure [nextAddr, nextInstAddr nextAddr]
-  where nextAddr = nextInstAddr curAddr
+  where
+    nextAddr = nextInstAddr curAddr
 nextAddresses curAddr (InstSkup _) =
   pure [nextAddr, nextInstAddr nextAddr]
-  where nextAddr = nextInstAddr curAddr
+  where
+    nextAddr = nextInstAddr curAddr
 nextAddresses curAddr (InstLoadI addr) = do
   put (TraceState (Just addr))
   pure [nextInstAddr curAddr]
 nextAddresses _ (InstJumpI (Address addr)) = do
-  TraceState { traceI = maybeAddr0 } <- get
+  TraceState {traceI = maybeAddr0} <- get
   case maybeAddr0 of
     Nothing -> throwError "JUMPI without previous LOADI"
     Just (Address addr0) ->
